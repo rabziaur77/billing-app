008bd6abfbe703988c4fdd3f3b11a56dce366574
Ziaur Rab <rabziaur77@outlook.com>
2025-06-24 17:41:25 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,10 @@
-import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Login from './Components/Account/Login'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useEffect } from 'react';
-
-function RedirectToPreservedPage() {
-  const navigate = useNavigate();
-
-  useEffect(()=>{
-    const path = sessionStorage.redirect;
-
-    if (path) {
-      sessionStorage.removeItem('redirect');
-      navigate(path, { replace: true });
-    }
-
-  },[navigate])
-
-  return null;
-}
 
 function App() {
   return (
     <BrowserRouter>
-      <RedirectToPreservedPage />
       <Routes>
         <Route path="/" element={<Login/>} />
         <Route path="/dashboard" element={<h2>Dashboard</h2>} />
