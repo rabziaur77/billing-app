54f5309499347b2bb293051e2fdd16c3901a658d
Ziaur Rab <rabziaur77@outlook.com>
2025-09-28 03:00:18 +0530

    update code

--- src/Service/API/API_Service.tsx
+++ src/Service/API/API_Service.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { clearStoredToken, getStoredToken, isTokenExpired, setStoredToken } from "./AuthService"
 
-const API_BASE_URL = "https://newbillingapigateway.onrender.com/";
+const API_BASE_URL = "https://billingapigateway.accurateappsolution.com/";
 //const API_BASE_URL = "http://localhost:5281/";
 
 export const API_SERVICE = axios.create({
